f0a3a3b0e517175c13f26d8e4e621a08756a3e3c
ibrahimlawal <ibrahim@lawal.me>
2016-03-01 13:51:38 +0100

    Added tests for getHTMLBody - Added clearer barriers and barriers between the different output   options in a message

--- tests/run_tests.php
+++ tests/run_tests.php
@@ -4,11 +4,18 @@
 // php run_tests.php
 
 
-// added a clearer barrier
+// added a clear barrier between messages
 function printBarrier()
 {
     echo "\r\n";
     echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=";
+    echo "\r\n";
+}
+// add in-message barrier
+function printInMessageBarrier()
+{
+    echo "\r\n";
+    echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
     echo "\r\n";
 }
 function printnl($message)
@@ -16,16 +23,25 @@ function printnl($message)
     echo "$message\r\n";
 }
 
+// include the PlancakeEmailParser class
 require_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . "PlancakeEmailParser.php");
 
+// fetch all sample emails
 $emails = glob(__DIR__ . DIRECTORY_SEPARATOR . "emails" . DIRECTORY_SEPARATOR . "*");
 
+// start output with a barrier
 printBarrier();
 
+// after correction, getHTMLBody doesnt include barrier in output
 foreach ($emails as $email) {
     printnl("Email $email");
     $emailParser = new PlancakeEmailParser(file_get_contents($email));
     printnl("subject: " . $emailParser->getSubject());
+    printInMessageBarrier();
     printnl("body: " . $emailParser->getBody());
+    printInMessageBarrier();
+    printnl("plain body: " . $emailParser->getPlainBody());
+    printInMessageBarrier();
+    printnl("html body: " . $emailParser->getHTMLBody());
     printBarrier();
 }
