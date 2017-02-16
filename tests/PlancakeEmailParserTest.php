96a522379ff9b06ba3906a1a81a5568fe1a5b1cc
Martijn Braam <martijn@brixit.nl>
2017-02-16 15:10:23 +0100

    Added test case for plaintext body

--- tests/PlancakeEmailParserTest.php
+++ tests/PlancakeEmailParserTest.php
@@ -87,4 +87,18 @@ class PlancakeEmailParserTest extends TestCase {
       $this->assertEquals($answers['sender'], $email->getSender());
     }
   }
+
+  public function testPlainBody() {
+    foreach (glob(__DIR__ . '/emails/*.txt') as $testFile) {
+
+      $answerFile = str_replace('.txt', '.yml', $testFile);
+      $answers = \Symfony\Component\Yaml\Yaml::parse(
+        file_get_contents($answerFile)
+      );
+
+      $email = new PlancakeEmailParser(file_get_contents($testFile));
+
+      $this->assertEquals($answers['plainbody'], $email->getPlainBody());
+    }
+  }
 }
