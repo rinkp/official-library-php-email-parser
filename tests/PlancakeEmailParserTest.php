8b2a644bd5f8650b2241df1802772dea717859e5
Martijn Braam <martijn@brixit.nl>
2017-02-16 14:48:20 +0100

    Created first test case

---
+++ tests/PlancakeEmailParserTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+use PHPUnit\Framework\TestCase;
+
+class PlancakeEmailParserTest extends TestCase {
+  public function testSubject() {
+    foreach (glob(__DIR__ . '/emails/*.txt') as $testFile) {
+
+      $answerFile = str_replace('.txt', '.yml', $testFile);
+      $answers = \Symfony\Component\Yaml\Yaml::parse(
+        file_get_contents($answerFile)
+      );
+
+      $email = new PlancakeEmailParser(file_get_contents($testFile));
+
+      $this->assertEquals($answers['subject'], $email->getSubject());
+    }
+  }
+}
