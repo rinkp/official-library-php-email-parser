72c60cbf6e0400a8faf5cfd1859461a9a7fe1614
Martijn Braam <martijn@brixit.nl>
2017-02-16 14:54:14 +0100

    Added testcase for "From: "

--- tests/PlancakeEmailParserTest.php
+++ tests/PlancakeEmailParserTest.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 use PHPUnit\Framework\TestCase;
 
 class PlancakeEmailParserTest extends TestCase {
+
   public function testSubject() {
     foreach (glob(__DIR__ . '/emails/*.txt') as $testFile) {
 
@@ -14,6 +15,20 @@ class PlancakeEmailParserTest extends TestCase {
       $email = new PlancakeEmailParser(file_get_contents($testFile));
 
       $this->assertEquals($answers['subject'], $email->getSubject());
+    }
+  }
+
+  public function testFrom() {
+    foreach (glob(__DIR__ . '/emails/*.txt') as $testFile) {
+
+      $answerFile = str_replace('.txt', '.yml', $testFile);
+      $answers = \Symfony\Component\Yaml\Yaml::parse(
+        file_get_contents($answerFile)
+      );
+
+      $email = new PlancakeEmailParser(file_get_contents($testFile));
+
+      $this->assertEquals($answers['from'], $email->getFrom());
     }
   }
 }
